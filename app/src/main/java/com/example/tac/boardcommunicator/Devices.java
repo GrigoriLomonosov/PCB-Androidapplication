d6260b33419c955a8325377683d3696e1a9c9743
unknown <demeyer.jeroen@hotmail.com>
2018-08-13 17:27:08 +0430

    HC-05 displayed correctly on bluetooth screen

--- app/src/main/java/com/example/tac/boardcommunicator/Devices.java
+++ app/src/main/java/com/example/tac/boardcommunicator/Devices.java
@@ -12,72 +12,83 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Devices extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener{
 
+    // Layout fields
     private String TAG = "test";
-    private TextView text;
-    private Button btn;
+    private TextView device_name;
+    private TextView device_address;
+    private Button connectBtn;
+
+    // Bluetooth fields
+    private Set<BluetoothDevice> devices = new HashSet<>();
+    private BluetoothDevice[] deviceArr;
 
     String[] country = { "India", "USA", "China", "Japan", "Other"};
 
     private BluetoothAdapter myBluetooth = null;
-    private Set<BluetoothDevice> pairedDevices;
-    public static String EXTRA_ADDRESS = "device_address";
-
-    private List<BlueToothDevice> devices = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //TODO remove
-        devices.add(new BlueToothDevice("name1","address1"));
-        devices.add( new  BlueToothDevice("name2","address2"));
 
-        //TextView text = (TextView) findViewById(R.id.textView);
-
+        // Make the Bluetooth connection and create the paired devices
         myBluetooth = BluetoothAdapter.getDefaultAdapter();
         if ( myBluetooth==null ) {
-            //text.setText("adapter niet gevonden");
+            Log.d(TAG, "onCreate: BTAdapter niet gevonden");
 
         } else if ( !myBluetooth.isEnabled() ) {
             Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnBTon, 1);
-            //text.setText(myBluetooth.toString());
+            Log.d(TAG, "onCreate: BTAdapter gevonden");
         }
+        //TODO fill the devices array
+        devices = myBluetooth.getBondedDevices();
+        deviceArr = setToArr(devices);
 
         setContentView(R.layout.activity_devices);
 
-        text = (TextView) findViewById(R.id.devices_text_view);
-        text.setText("test");
+        device_name = (TextView) findViewById(R.id.device_name);
+        device_address = (TextView) findViewById(R.id.device_address);
 
         // Spinner with devices
-        String[] arraySpinner = new String[] {
-                "1", "2", "3", "4", "5"
-        };
         Spinner s = (Spinner) findViewById(R.id.BTSpinner);
         s.setOnItemSelectedListener(this);
-        //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Setting the ArrayAdapter data on the Spinner
-        s.setAdapter(aa);
+        //Creating the ArrayAdapter instance having the devices list
 
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,deviceArr);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(aa);
 
         super.onCreate(savedInstanceState);
 
     }
 
+    private BluetoothDevice[] setToArr(Set<BluetoothDevice> input){
+        BluetoothDevice[] result = new BluetoothDevice[input.size()];
+        int i = 0;
+        for(BluetoothDevice dev: input){
+            result[i] = dev;
+            i++;
+        }
+        return result;
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-        Toast.makeText(getApplicationContext(),country[position] , Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(),country[position] , Toast.LENGTH_LONG).show();
+        device_address.setText(deviceArr[position].getAddress());
+        device_name.setText(deviceArr[position].getName());
     }
 
     @Override
