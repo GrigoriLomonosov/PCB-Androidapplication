60829250e8b50ff76f26ab18e4d2da1558aae9e4
unknown <demeyer.jeroen@hotmail.com>
2018-08-15 13:06:26 +0430

    Added a singleton

--- app/src/main/java/com/example/tac/boardcommunicator/BluetoothService.java
+++ app/src/main/java/com/example/tac/boardcommunicator/BluetoothService.java
@@ -3,60 +3,30 @@ package com.example.tac.boardcommunicator;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-/**
- * Searches for Bluetooth devices and allows connection and disconnection of a selected device
- */
-public class Devices extends AppCompatActivity implements
-        AdapterView.OnItemSelectedListener{
+public class BluetoothService {
+
+    //TODO TESTfield
+    private String TAG = "test";
 
     // Standard UUID
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
-    // Layout fields
-    private String TAG = "test";
-    private TextView device_name;
-    private TextView device_address;
-    private TextView result_text;
+    // Static field to allow for the SINGLETON design pattern
+    private static BluetoothService INSTANCE = null;
 
-    private BluetoothDevice device;
-
-    // The Bluetooth service
-    private final BluetoothService service = BluetoothService.getInstance();
-
-/*    // Bluetooth fields
+    // Bluetooth fields
     private Set<BluetoothDevice> devices = new HashSet<>();
     private BluetoothDevice[] deviceArr;
     private BluetoothDevice device;
@@ -66,96 +36,51 @@ public class Devices extends AppCompatActivity implements
     // Keeps track of currently running threads
     private ConnectThread connectThread;
     private ConnectedThread connectedThreadRead;
-    private ConnectedThread connectedThreadWrite;*/
+    private ConnectedThread connectedThreadWrite;
 
-    // Create a BroadcastReceiver for ACTION_FOUND of new devices.
-    /*private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                //TODO handle discovery of new objects
-            }
-        }
-    };*/
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        // Make objects necessary for BT-connection and fill devices array
-       /* myBluetooth = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothService() {
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
         if ( myBluetooth==null ) {
             Log.d(TAG, "onCreate: BTAdapter niet gevonden");
 
         } else if ( !myBluetooth.isEnabled() ) {
             Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(turnBTon, 1);
-            Log.d(TAG, "onCreate: BTAdapter gevonden");
         }
         //TODO fill the devices array
         devices = myBluetooth.getBondedDevices();
-        deviceArr = setToArr(devices);*/
-
-        // Register for broadcasts when a device is discovered. TODO
-        // Register for broadcasts when a device is discovered.
-       /* IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter);*/
-
-        setContentView(R.layout.activity_devices);
-
-        // Set the layout
-        device_name = (TextView) findViewById(R.id.device_name);
-        device_address = (TextView) findViewById(R.id.device_address);
-        result_text = (TextView) findViewById(R.id.BT_result_text);
-        Spinner s = (Spinner) findViewById(R.id.BTSpinner);
-        s.setOnItemSelectedListener(this);
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,service.getDevices());
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(aa);
-
-        super.onCreate(savedInstanceState);
+        deviceArr = setToArr(devices);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-        Log.d(TAG, "onItemSelected: in selecter");
-        device = service.getDevices()[position];
-        service.setDevice(device);
-        if(device != null){
-            device_address.setText(device.getAddress());
-            device_name.setText(device.getName());
+    public static BluetoothService getInstance(){
+        if (INSTANCE == null) {
+            INSTANCE = new BluetoothService();
         }
+        return(INSTANCE);
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> arg0) {
-        device_address.setText("");
-        device_name.setText("");
+    public BluetoothDevice[] getDevices(){
+        return deviceArr;
     }
 
-    public void connectToBT(View view){
-        Log.d(TAG, "connectToBT: pressed");
-        service.connectToBT();
+    public void setDevice(BluetoothDevice device){
+        this.device = device;
     }
 
-    public void disconnectFromBT(View view){
-        Log.d(TAG, "disconnectFromBT: pressed");
-        service.disconnectFromBT();
+    public BluetoothDevice getDevice(){
+        return device;
     }
-/*
-    *//**
+
+
+    /**
      * Connects the application to the selected Bluetooth device. Connection is only possible when there is not yet a connection established and a device selected
-     * @param view
-     *//*
-    public void connectToBT(View view){
+     */
+    public void connectToBT(){
         if (device != null && connectThread == null){
             connectThread = new ConnectThread(device);
             connectThread.run();
             connectedThreadRead = new ConnectedThread();
             connectedThreadWrite = new ConnectedThread();
-            result_text.setText("Connection successfull");
+            //result_text.setText("Connection successfull");
             //Start listening in the background to the connected device
             AsyncTask.execute(new Runnable() {
                 @Override
@@ -167,20 +92,19 @@ public class Devices extends AppCompatActivity implements
         else{
             Log.d(TAG, "connectToBT: Connect to BT failed");
             if (connectThread != null) {
-                result_text.setText("Already connected to a device. Disconnect before trying to connect again.");
+                //result_text.setText("Already connected to a device. Disconnect before trying to connect again.");
             }
             else if(device == null){
-                result_text.setText("Please select a device before trying to connect.");
+                //result_text.setText("Please select a device before trying to connect.");
             }
             //TODO write alert to tell users to select a device
         }
     }
 
-    *//**
+    /**
      * Disconnects the application from the current Bluetooth device.
-     * @param view
-     *//*
-    public void disconnectFromBT(View view){
+     */
+    public void disconnectFromBT(){
         //Stop connection thread
         if(connectThread != null){
             connectThread.cancel();
@@ -197,7 +121,7 @@ public class Devices extends AppCompatActivity implements
         }
         connectedThreadWrite = null;
         if(connectThread == null && connectedThreadRead == null && connectedThreadWrite == null){
-            result_text.setText("No more connections: disconnection successfull");
+            //result_text.setText("No more connections: disconnection successfull");
         }
     }
 
@@ -205,7 +129,7 @@ public class Devices extends AppCompatActivity implements
         //TODO Hoe gaan we dat oplossen
         String result = "";
         if(connectedThreadRead == null){
-            result_text.setText("Please connect before trying to read");
+            //result_text.setText("Please connect before trying to read");
         }
         else{
             //result = new String(connectedThreadRead.read());
@@ -216,14 +140,15 @@ public class Devices extends AppCompatActivity implements
 
     public void write(String message){
 
-    }*/
-/*
+    }
+
+
     //##############################################################################################
     //##############################################################################################
 
-    *//**
+    /**
      * Handles the connection to a bluetooth device
-     *//*
+     */
     private class ConnectThread extends Thread {
         private final BluetoothDevice mmDevice;
 
@@ -239,9 +164,9 @@ public class Devices extends AppCompatActivity implements
             socket = temp;
         }
 
-        *//**
+        /**
          * Tries to setup a Bluetooth connection with a device
-         *//*
+         */
         public void run() {
             // Cancel discovery because it otherwise slows down the connection.
             myBluetooth.cancelDiscovery();
@@ -276,9 +201,9 @@ public class Devices extends AppCompatActivity implements
     //##############################################################################################
     //##############################################################################################
 
-    *//**
+    /**
      * Handles the sending and receiving of data to/from the Bluetooth device
-     *//*
+     */
     private class ConnectedThread extends Thread {
         private final int buffersize = 256;
         private final InputStream mmInStream;
@@ -306,9 +231,9 @@ public class Devices extends AppCompatActivity implements
             mmOutStream = tmpOut;
         }
 
-        *//**
+        /**
          * Listens to the connected Bluetooth device for incoming messages.
-         *//*
+         */
         public void run() {
             mmBuffer = new byte[buffersize];
 
@@ -346,13 +271,13 @@ public class Devices extends AppCompatActivity implements
                 Log.e(TAG, "Could not close the connect socket", e);
             }
         }
-    }*/
+    }
 
-/*    *//**
+    /**
      * Alters set of Bluetoothdevices to an array of Bluetoothdevices
      * @param input
      * @return
-     *//*
+     */
     private BluetoothDevice[] setToArr(Set<BluetoothDevice> input){
         BluetoothDevice[] result = new BluetoothDevice[input.size()];
         int i = 0;
@@ -361,5 +286,5 @@ public class Devices extends AppCompatActivity implements
             i++;
         }
         return result;
-    }*/
+    }
 }
